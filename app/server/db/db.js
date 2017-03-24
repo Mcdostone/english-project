c5cc441248cbceae344ed188f813aa22ad9c7be6
Mcdostone <yann.prono@telecomnancy.net>
2017-03-24 12:14:09 +0100

    HMM

--- app/server/db/db.js
+++ app/server/db/db.js
@@ -1,15 +1,29 @@
 let Sequelize = require('sequelize')
 let config = require('../config')
 
-let sequelize = new Sequelize(config.database, config.username, config.password, {
-  logging: false,
-  host: 'localhost',
-  dialect: 'postgres',
-  pool: {
-    max: 5,
-    min: 0,
-    idle: 10000
-  }
-})
+let sequelize = undefined
+
+if (process.env.DATABASE_URL) {
+  // the application is executed on Heroku ... use the postgres database
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect:  'postgres',
+    protocol: 'postgres',
+    logging:  true
+  })
+}
+else {
+  sequelize = new Sequelize(config.database, config.username, config.password, {
+    logging: false,
+    host: 'localhost',
+    dialect: 'postgres',
+    pool: {
+      max: 5,
+      min: 0,
+      idle: 10000
+    }
+  })
+
+}
+
 
 module.exports = sequelize
